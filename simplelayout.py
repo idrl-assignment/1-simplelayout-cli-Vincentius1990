63943a41ed5b8f2ea049205ce71ed5af189ec7df
Vincentius <vincent1990@126.com>
2020-11-11 20:53:54 +0800

    79

--- simplelayout.py
+++ simplelayout.py
@@ -4,8 +4,8 @@ from pathlib import path
 
 
 def main():
-    parser = argparse.ArgumentParser()  # 创建一个ArgumentParser对象
-    parser.add_argument("--board_grid", type=int,  # 调用add_argument()方法给ArgumentParser添加程序参数信息
+    parser = argparse.ArgumentParser()  
+    parser.add_argument("--board_grid", type=int,  
                         help="the board_grid")
     parser.add_argument("--unit_grid", type=int,
                         help="the unit_grid")
@@ -21,7 +21,8 @@ def main():
 
     if args.board_grid % args.unit_grid != 0:
         sys.exit('不能整除')
-    elif len(args.positions) != args.unit_n | args.positions < 1 | args.positions > (args.board_grid/args.unit_grid)**2:
+    elif len(args.positions) != args.unit_n | args.positions < 1 | \
+        args.positions > (args.board_grid/args.unit_grid)**2:
         sys.exit('组件编号有误')
     else:
         path(args.outdir + args.file_name + '.mat')
