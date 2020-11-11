a32c0a31db5130638a26505c188fc91bbaf42ec5
Vincentius <vincent1990@126.com>
2020-11-11 20:28:50 +0800

    try

--- simplelayout.py
+++ simplelayout.py
@@ -4,29 +4,28 @@ import os
 
 
 def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--board_grid", type=int, 
+    parser = argparse.ArgumentParser()       #创建一个ArgumentParser对象
+    parser.add_argument("--board_grid", type=int,     #调用add_argument()方法给ArgumentParser添加程序参数信息
                         help="the board_grid")
     parser.add_argument("--unit_grid", type=int,
                         help="the unit_grid")
     parser.add_argument("--unit_n", type=int,
                         help="the number_of_unit")
-    parser.add_argument("--positions", type=int, 
+    parser.add_argument("--positions", type=int, nargs='+',
                         help="the positions")                       
-    parser.add_argument("-o", "--outdir", type=str,
+    parser.add_argument("-o", "--outdir", type=str, default='/example_dir',
                         help="the outdir")
     parser.add_argument("--file_name", type=str, default='example',
                         help="the file_name")
     args = parser.parse_args()
-    n = args.board_grid/args.unit_grid
+
     if args.board_grid % args.unit_grid != 0:
-        sys.exit()
-    elif args.positions < 1 | args.positions > n**2:
-        sys.exit()
+        sys.exit('不能整除')
+    elif len(args.position) != args.unit_n | args.positions < 1 | args.positions > (args.board_grid/args.unit_grid)**2 :
+        sys.exit('组件编号有误')
     else:
-        path = r'D:/test'
-        os.mkdir(path + args.outdir + args.file_name + '.mat',
-                 path + args.outdir + args.file_name + '.jpg')
+        os.mkdir(args.outdir + args.file_name + '.mat',
+                 args.outdir + args.file_name + '.jpg')
 
 
 if __name__ == "__main__":
